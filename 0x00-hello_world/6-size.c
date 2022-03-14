7936467894fb8c59806b24eb860470f36c84f89e
samdeldev <abidemiadetogun@gmail.com>
2022-03-14 00:34:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <float.h>
 /**
  * main - entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char alx_char = 'A';
+	int alx_int = 3;
+	long int alx_longI;
+	long long int alx_long_longI;
+	float alx_float = 2.4;
+
 	printf("Size of a char: %d byte(s)\n", Sizeof(char));
 	printf("Size of an int: %d byte(s)\n", Sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", Sizeof(long int));
