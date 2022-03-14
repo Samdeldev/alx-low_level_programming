1ee9d77e8035050c6f7300ce7bba7c599d7d9766
samdeldev <abidemiadetogun@gmail.com>
2022-03-14 09:05:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <limits.h>
-#include <float.h>
+
 /**
- * main - entry point
- * Return: it must be a value for 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alx_char = 'A';
-	int alx_int = 3;
-	long int alx_longI;
-	long long int alx_long_longI;
-	float alx_float = 2.4;
-
-	printf("Size of a char: %d byte(s)\n", Sizeof(char));
-	printf("Size of an int: %d byte(s)\n", Sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", Sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", Sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", Sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int);
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
